a31957baca5116ea9d586bb0b80b9eb92ac5a906
Morley Tatro <morleytatro@gmail.com>
2020-12-01 12:43:49 -0800

    initial commit

---
+++ Week1/DataStructures/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            for(int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+
+            string[] myArray = new string[4]
+            {
+                "this",
+                "that",
+                "the other",
+                "finally"
+            };
+
+            // myArray[0] = "this";
+
+            // for(int i = 0; i < myArray.Length; i++)
+            // {
+            //     Console.WriteLine(myArray[i]);
+            // }
+
+            // alternative way of loop through array
+            // foreach(var word in myArray)
+            // {
+            //     Console.WriteLine(word);
+            // }
+
+            object[] myObjectsArray = 
+            {
+                "this",
+                "that",
+                1,
+                true
+            };
+
+            // foreach(object element in myObjectsArray)
+            // {
+            //     if(element is string)
+            //     {
+            //         Console.WriteLine(element);
+            //     }
+            // }
+
+            List<string> names = new List<string>() {
+                "Steven",
+                "Jason",
+                "Steven"
+            };
+
+            names.Add("Quinton");
+
+            names.Remove("Steven");
+
+            names.RemoveAt(1);
+
+            foreach(var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Dictionary<string, object> myBook = new Dictionary<string, object>();
+
+            myBook.Add("Title", "Infinite Jest");
+            myBook.Add("Author", "David Foster Wallace");
+            myBook.Add("Pages", 1079);
+            myBook.Add("Genre", "surrealist satire");
+            myBook.Add("Rating", 5);
+
+            // my_book = {
+            //     "title": "Infinite Jest",
+            //     "author": "David Foster Wallace"
+            // }
+            myBook["Rating"] = 4;
+
+            Dictionary<string, object> book2 = myBook;
+            book2["Pages"] = 1000;
+
+            Console.WriteLine(myBook["Pages"]);
+        }
+    }
+}
---
+++ Week1/OOPIntro/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace OOPIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person morley = new Person("Morley", "Tatro");
+
+            // invoking our "setter" method
+            morley.HairColor = "gray";
+            // Console.WriteLine(morley);
+
+            // this calls the get accessor for us
+            Console.WriteLine(morley.FullName);
+
+            Console.WriteLine(morley.HairColor);
+
+            Person j = new Person("Jason", "Irvin");
+            Console.WriteLine(j);
+
+            // this causes an issue because the implicit access level is private
+            // morley.firstName = "Morley";
+        }
+    }
+
+    // Classes allow us to model our data.
+    // Classes act as blueprints to create objects.
+    class Person
+    {
+        // attributes are things that objects can have
+        // methods are things that objects an do
+
+        // these are fields, which typically have camel casing
+        // string firstName;
+        // string lastName;
+
+        // string hairColor;
+
+        // these are properties (notice the PascalCasing)
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+
+        // effectively read only because we can only "get" it
+        // it is derived
+        public string FullName {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+        public string HairColor {get;set;}
+
+        // this is our constructor
+        public Person(string first, string last, string hair = "black")
+        {
+            FirstName = first;
+            LastName = last;
+            HairColor = hair;
+        }
+
+        // def __str__(self)
+        // overriding the built-in method to get a string representation
+        public override string ToString()
+        {
+            return $"The person's name is {FirstName} {LastName} and has {HairColor} hair.";
+        }
+
+        // this is a setter method (allows us to access private fields)
+        // public void SetHairColor(string newColor)
+        // {
+        //     hairColor = newColor;
+        // }
+
+        // this is a getter method (allows us to access private fields)
+        // public string GetHairColor()
+        // {
+        //     return hairColor;
+        // }
+    }
+}
