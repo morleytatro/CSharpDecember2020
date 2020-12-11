3ae9001847bd638d23a9f7f83e9a341f5a48af7a
Morley Tatro <morleytatro@gmail.com>
2020-12-10 16:28:01 -0800

    added CRUD methods and pages

--- Week2/FirstDB/Controllers/HomeController.cs
+++ Week2/FirstDB/Controllers/HomeController.cs
@@ -21,9 +21,86 @@ namespace FirstDB.Controllers
             _context = context;
         }
 
+        [HttpGet("")]
         public IActionResult Index()
         {
+            ViewBag.AllStudents = _context.Students
+                // note that the most recently added student will be on top
+                .OrderByDescending(student => student.CreatedAt)
+                .ToList();
+
             return View();
+        }
+
+        [HttpGet("students/new")]
+        public IActionResult NewStudentPage()
+        {
+            return View();
+        }
+
+        [HttpPost("students")]
+        public IActionResult CreateStudent(Student studentToCreate)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("NewStudentPage");
+            }
+
+            _context.Add(studentToCreate);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("students/{id}/edit")]
+        public IActionResult EditStudentPage(int id)
+        {
+            var studentToEdit = _context
+                .Students
+                .Find(id);
+
+            return View(studentToEdit);
+        }
+
+        [HttpPost("students/{id}/update")]
+        public IActionResult UpdateStudent(Student updatedStudent, int id)
+        {
+            if(!ModelState.IsValid)
+            {
+                // reconnecting the ID since the student object didn't have one
+                // this could be changed by using a hidden input
+                updatedStudent.StudentId = id;
+                return View("EditStudentPage", updatedStudent);
+            }
+
+            var studentToUpdate = _context
+                .Students
+                .Find(id);
+            
+            studentToUpdate.FirstName = updatedStudent.FirstName;
+            studentToUpdate.LastName = updatedStudent.LastName;
+            studentToUpdate.Email = updatedStudent.Email;
+            studentToUpdate.FavoriteNumber = updatedStudent.FavoriteNumber;
+            studentToUpdate.UpdatedAt = DateTime.Now;
+
+            // just like in Django!
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        // coming from the asp-route-id in the view
+        [HttpPost("students/{id}/delete")]
+        public IActionResult DeleteStudent(int id)
+        {
+            var studentToDelete = _context
+                .Students
+                .Find(id);
+
+            _context.Students.Remove(studentToDelete);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
--- Week2/FirstDB/Models/Student.cs
+++ Week2/FirstDB/Models/Student.cs
@@ -8,16 +8,20 @@ namespace FirstDB.Models
         [Key]
         public int StudentId {get;set;}
 
+        [Required(ErrorMessage="Please enter the first name.")]
+        [Display(Name="First Name")]
         public string FirstName {get;set;}
 
+        [Display(Name="Last Name")]
         public string LastName {get;set;}
 
         public string Email {get;set;}
 
+        [Display(Name="Favorite Number")]
         public int FavoriteNumber {get;set;}
 
-        public DateTime CreatedAt {get;set;} = DateTime.UtcNow;
+        public DateTime CreatedAt {get;set;} = DateTime.Now;
 
-        public DateTime UpdatedAt {get;set;} = DateTime.UtcNow;
+        public DateTime UpdatedAt {get;set;} = DateTime.Now;
     }
 }
